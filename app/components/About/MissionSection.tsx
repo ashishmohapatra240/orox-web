9913c85544653199c88bb5d06d430a9eb7e44bbf
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-03-20 15:36:22 +0530

    Update MissionSection component

--- app/components/About/MissionSection.tsx
+++ app/components/About/MissionSection.tsx
@@ -1,24 +1,22 @@
 export const MissionSection = () => {
   return (
-    <section className="w-full bg-[#EAF5FF] px-4 py-24">
+    <section className="w-full bg-[#EAF5FF] px-[16px] py-[48px] md:px-[280px] md:py-[88px]">
       <div className="mx-auto max-w-7xl">
-        <div className="mx-auto max-w-4xl text-center">
+        <div className="text-center">
           <span className="text-[16px] font-semibold bg-clip-text text-transparent bg-gradient-to-b from-[#276EDE] to-[#11032F] text-center mb-[8px]">
             OROX&apos;S MISSION
           </span>
-          <h2 className="text-[40px] font-bold text-[#19191B] md:text-[56px] leading-[68px]">
-            Making pro-level investing
-            <br />
-            accessible to everyone
+          <h2 className="text-[34px] font-bold text-[#19191B] md:text-[56px] leading-[40px] md:leading-[68px] w-[340px] md:w-[900px] mx-auto mt-[8px]">
+            Making pro-level investing accessible to everyone
           </h2>
           <div className="mt-[24px]">
-            <p className="text-[20px] text-[#19191B] leading-[32px]">
+            <p className="text-[16px] md:text-[20px] text-[#19191B] leading-[24px] md:leading-[32px]">
               Our mission is simple: to make pro-level investing accessible to
               everyone. We create secure, transparent, automated investment
               solutions for everyday investors and institutions by combining AI,
               blockchain and quantitative finance.
             </p>
-            <p className="text-[20px] text-[#19191B] leading-[32px] mt-[24px]">
+            <p className="text-[16px] md:text-[20px] text-[#19191B] leading-[24px] md:leading-[32px] mt-[16px] md:mt-[24px]">
               We believe in financial empowerment—helping people grow their
               wealth without the complexity of traditional investing.
             </p>
