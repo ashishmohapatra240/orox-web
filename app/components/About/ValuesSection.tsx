aaaccf0fe16f3044fdf843292e87cdfdc667c106
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-03-20 16:30:22 +0530

    Refine typography and leading in ValuesSection component

--- app/components/About/ValuesSection.tsx
+++ app/components/About/ValuesSection.tsx
@@ -50,10 +50,10 @@ export const ValuesSection = () => {
                     className="object-contain transition-transform duration-300 hover:scale-110"
                   />
                 </div>
-                <h3 className="mb-2 text-[20px] md:text-[28px] font-bold text-[#19191B] mt-[24px] leading-[40px]">
+                <h3 className="mb-[8px] text-[20px] md:text-[28px] font-bold text-[#19191B] mt-[24px] leading-[32px] md:leading-[40px]">
                   {value.title}
                 </h3>
-                <p className="text-[#19191B] text-[16px] md:text-[20px] leading-[32px] mt-[8px]">
+                <p className="text-[#19191B] text-[16px] md:text-[20px] leading-[24px] md:leading-[32px] mt-[8px]">
                   {value.description}
                 </p>
               </div>
