0a07601f57a9bedde298d2f2de74d955d9812740
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-04-03 13:25:48 +0530

    Enhance BlogCard component styling with hover effects, update 'Read more' section to use SVG icon

--- app/components/Blog/BlogCard.tsx
+++ app/components/Blog/BlogCard.tsx
@@ -16,10 +16,10 @@ export const BlogCard = ({
   return (
     <Link
       href={link}
-      className="group space-y-4 w-[295px] md:w-[356px] flex-shrink-0"
+      className="group space-y-4 w-[295px] md:w-[356px] flex-shrink-0 hover:shadow-[0px_24px_16px_-10px_#0000001A] p-[16px] transition-all duration-300 rounded-[32px]"
     >
       {/* Image Container */}
-      <div className="relative overflow-hidden rounded-3xl w-[295px] h-[372px] md:w-[356px] md:h-[448px]">
+      <div className="relative overflow-hidden rounded-[32px] w-[295px] h-[372px] md:w-[356px] md:h-[448px] ">
         <Image
           src={image}
           alt={title}
@@ -36,14 +36,23 @@ export const BlogCard = ({
         <p className="text-[20px] text-[#19191B] leading-[32px] mt-2">
           {description}
         </p>
-        <div className="flex items-center space-x-2 text-[#2F5DFD] mt-[24px]">
-          <span className="text-[14px] font-bold ">Read more</span>
-          <Image
-            src="/icons/arrow.svg"
-            alt="Read more"
-            width={14}
-            height={14}
-          />
+        <div className="flex items-center space-x-2 text-[#2F5DFD] mt-[24px] hover:text-[#21275B] hover:bg-[#21275B]/10 rounded-[8px] p-[8px] transition-colors duration-100 w-fit">
+          <span className="text-[14px] font-bold transition-colors duration-100">
+            Read more
+          </span>
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="transition-colors duration-100"
+          >
+            <path
+              d="M6.75 3.75V5.25H11.6925L3 13.9425L4.0575 15L12.75 6.3075V11.25H14.25V3.75H6.75Z"
+              fill="currentColor"
+            />
+          </svg>
         </div>
       </div>
     </Link>
