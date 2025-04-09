d943f78d8807797ba341a382eea5bcda6fd8b25d
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-04-09 15:39:33 +0530

    Add ProductODNCalculator component and integrate it into ProductsOdn page. Update Button component to support 'link' variant and add new Input component. Include new SVG icons for UI elements.

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,8 +1,8 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
-
+import Image from "next/image";
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  variant?: "primary" | "secondary" | "white" | "outline";
+  variant?: "primary" | "secondary" | "white" | "outline" | "link";
 }
 
 export const Button = ({
@@ -24,6 +24,8 @@ export const Button = ({
             ? "self-stretch px-4 py-4 bg-white rounded-[32px] inline-flex justify-center items-center gap-2 text-[#293483] text-[16px] md:text-[20px] font-bold"
             : variant === "outline"
             ? "self-stretch px-4 py-4 bg-transparent rounded-[32px] border-2 border-[#78CAB9] inline-flex justify-center items-center gap-2 text-white font-bold text-[16px] md:text-[20px]"
+            : variant === "link"
+            ? "inline-flex items-center gap-2 text-[#2F5DFD] hover:text-blue-500 font-bold"
             : ""
         }
         ${className}
@@ -31,6 +33,14 @@ export const Button = ({
       {...props}
     >
       {children}
+      {variant === "link" && (
+        <Image
+          src="/icons/ic_north_east.svg"
+          alt="Arrow right"
+          width={24}
+          height={24}
+        />
+      )}
     </button>
   );
 };
