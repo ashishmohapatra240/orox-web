8355be9b573c22b51cf6fd3e92ed38a42d86c2b1
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-04-10 02:45:56 +0530

    Refactor Products Section

--- app/(webpages)/products-odn/page.tsx
+++ app/(webpages)/products-odn/page.tsx
@@ -2,13 +2,14 @@ import { InvestingSection } from "@/app/components/About/InvestingSection";
 import { ProductODNCalculator } from "@/app/components/ProductODN/ProductODNCalculator";
 import { ProductODNFeatures } from "@/app/components/ProductODN/ProductODNFeatures";
 import ProductOdnHero from "@/app/components/ProductODN/ProductODNHero";
-
+import { ProductOroxProducts } from "@/app/components/ProductODN/ProductOroxProducts";
 export default function ProductsOdn() {
   return (
     <div>
       <ProductOdnHero />
       <ProductODNFeatures />
       <ProductODNCalculator />
+      <ProductOroxProducts />
       <InvestingSection />
     </div>
   );
