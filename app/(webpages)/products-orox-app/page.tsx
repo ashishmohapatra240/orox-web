af02686f66c59d80a830357b58cd7badb1b1d740
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-04-10 02:44:11 +0530

    Add ProductOroxProducts component to display product and integrate video playback in ProductOroxHero. Include new assets for product images and video.

--- app/(webpages)/products-orox-app/page.tsx
+++ app/(webpages)/products-orox-app/page.tsx
@@ -1,6 +1,7 @@
 import { ProductOroxFeatures } from "@/app/components/ProductOrox/ProductOroxFeatures";
 import ProductOroxHero from "@/app/components/ProductOrox/ProductOroxHero";
 import { ProductOroxKeyFeatures } from "@/app/components/ProductOrox/ProductOroxKeyFeatures";
+import { ProductOroxProducts } from "@/app/components/ProductOrox/ProductOroxProducts";
 import Service from "@/app/components/Service/Service";
 import { Steps } from "@/app/components/Steps/Steps";
 
@@ -11,6 +12,7 @@ export default function ProductsOroxApp() {
       <ProductOroxFeatures />
       <ProductOroxKeyFeatures />
       <Steps />
+      <ProductOroxProducts />
       <Service />
     </div>
   );
---
+++ app/components/ProductOrox/ProductOroxProducts.tsx
@@ -0,0 +1,114 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { JSX } from "react";
+
+const products: {
+  tag: string;
+  title: JSX.Element;
+  description: string | JSX.Element;
+  image: string | JSX.Element;
+  imageAlt: string;
+  link: string;
+}[] = [
+  {
+    tag: "OUR PRODUCTS",
+    title: (
+      <>
+        <span className="relative inline-block z-10">
+          <span className="absolute left-0 top-[35%] -z-10 h-[80%] w-full bg-[#BBE5DC] rounded-[8px]"></span>
+          OROX
+        </span>{" "}
+        and a range of powerful products
+      </>
+    ),
+    description: (
+      <>
+        OROX is a seamless investment product offering low-risk growth and
+        crypto exposure with reduced volatility. Connect effortlessly to ODN and
+        the DVOX token—all in one app.
+      </>
+    ),
+    image: "/images/products/yield-chart.png",
+    imageAlt: "ODN Investment Chart",
+    link: "/products-odn",
+  },
+];
+
+export const ProductOroxProducts = () => {
+  return (
+    <section className="w-full bg-white px-[16px] py-[44px] md:px-[80px] md:py-[88px]">
+      <div className="mx-auto max-w-7xl">
+        {/* Products */}
+        <div className="space-y-8 sm:space-y-12 lg:space-y-44">
+          {products.map((product, index) => (
+            <div
+              key={index}
+              className="grid items-center gap-6 sm:gap-8 lg:gap-12 lg:grid-cols-2"
+            >
+              <div
+                className={`${
+                  index % 2 === 0 ? "lg:order-2 order-1" : "lg:order-1 order-2"
+                }`}
+              >
+                <span className="text-[14px] md:text-[16px] font-semibold bg-clip-text text-transparent bg-gradient-to-b from-[#4296E4] to-[#383699]">
+                  {product.tag}
+                </span>
+                <h2 className="text-[34px] md:text-[56px] font-bold text-[#19191B] leading-[48px] md:leading-[68px] mt-[8px]">
+                  {product.title}
+                </h2>
+                <p className="text-[16px] md:text-[20px] text-[#19191B] leading-[24px] md:leading-[32px] mt-[24px] mb-[40px]">
+                  {product.description}
+                </p>
+                <div>
+                  <Link
+                    href={product.link}
+                    className="inline-flex h-10 w-10 sm:h-11 sm:w-11 lg:h-12 lg:w-12 items-center justify-center rounded-full bg-[#293483] text-white transition-transform"
+                  >
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 24 24"
+                      className="w-5 h-5 sm:w-6 sm:h-6"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5 12H19M19 12L12 5M19 12L12 19"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+
+              <div
+                className={`relative w-full max-w-[400px] sm:max-w-[400px] lg:max-w-[600px] flex items-center ${
+                  index % 2 === 0 ? "lg:order-1" : "lg:order-2"
+                } lg:ml-auto lg:mr-0`}
+              >
+                <div className="relative aspect-[4/3] w-full overflow-hidden rounded-[32px] item">
+                  {typeof product.image === "string" ? (
+                    <Image
+                      src={product.image}
+                      alt={product.imageAlt}
+                      width={800}
+                      height={600}
+                      className="object-contain w-full h-full"
+                      priority
+                    />
+                  ) : (
+                    product.image
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
