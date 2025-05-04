47fcce509637792ce076546b3ed47592069c8fac
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-05-04 10:24:50 +0530

    Add Open Graph image support in metadata and include OG.png file

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,13 @@ export const metadata: Metadata = {
     "OROX Global – Expanding Investment Access with Innovation and Expertise",
   description:
     "OROX Global delivers quant-driven investment solutions that bridge traditional and emerging markets. Backed by expert teams and cutting-edge tech, we empower investors worldwide with smarter, risk-aware strategies.",
+  openGraph: {
+    images: [
+      {
+        url: "/OG.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
