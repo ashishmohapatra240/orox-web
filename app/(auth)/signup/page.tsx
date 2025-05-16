cede044b9fcfb5c938314797a89a55407fb677b0
Ashish <ashishmohapatra240@gmail.com>
2025-05-16 13:57:14 +0530

    Refactor login and signup pages: implement redirection to home and comment out component imports

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,12 @@
 // import { Signup } from "../../components/Auth/Signup";
+import { redirect } from "next/navigation";
 
 // export const metadata = {
 //   title: "Sign Up | OROX",
 //   description: "Create your OROX account and start investing",
 // };
 
-// export default function SignupPage() {
-//   return <Signup />;
-// }
+export default function SignupPage() {
+  redirect("/");
+  //   return <Signup />;
+}
